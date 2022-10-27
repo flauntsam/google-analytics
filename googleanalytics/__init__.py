83e10932b7839a61ea29d26ec8f301e3b936e5b7
JR Oakes <jroakes@gmail.com>
2022-10-27 10:39:24 -0400

    Update __init__.py

--- googleanalytics/__init__.py
+++ googleanalytics/__init__.py
@@ -4,6 +4,7 @@ import pkg_resources
 import re
 
 from . import auth, commands, tests, utils, account, auth, blueprint, columns, errors, query, segments
+# Authorize and Revoke no longer do anything.
 from .auth import authenticate, authorize, revoke
 from .blueprint import Blueprint
 
